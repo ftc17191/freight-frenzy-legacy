0967719b51a419de4352854192f00e43cb51bc2f
averagedolphin <t8wilhelm@gmail.com>
2021-11-13 17:38:06 -0600

    credited ftc lib and added getHeadingRadians

--- FtcLayer/src/main/java/org/ftc17191/ftclayer/hardware/imu/RevImu.java
+++ FtcLayer/src/main/java/org/ftc17191/ftclayer/hardware/imu/RevImu.java
@@ -7,22 +7,22 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
+// Thanks to FTC Lib,
+// We used FTC Lib's IMU class and the ConceptBNO015IMU as a reference when making this one.
+
 
 public class RevImu {
-    BNO055IMU imu;
 
-
+    public BNO055IMU imu;
 
     public RevImu(HardwareMap hmap, String id)
     {
         imu = hmap.get(BNO055IMU.class, id);
-    }
 
-    public void init() {
         BNO055IMU.Parameters param = new BNO055IMU.Parameters();
         param.angleUnit = BNO055IMU.AngleUnit.DEGREES;
         param.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-        param.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        param.calibrationDataFile = "BNO055IMUCalibration.json";
         param.loggingEnabled = true;
         param.loggingTag = "FTCLayer IMU";
         param.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
@@ -33,6 +33,11 @@ public class RevImu {
     public double getHeading()
     {
         return imu.getAngularOrientation().firstAngle;
-
     }
+
+    public double getHeadingRadians()
+    {
+        return Math.toRadians(imu.getAngularOrientation().firstAngle);
+    }
+
 }
